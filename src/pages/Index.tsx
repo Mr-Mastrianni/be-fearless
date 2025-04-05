3e868ff0da0dcde247a286addf31d1c825a51b5c
Corey Mastrianni <mastrianni11@gmail.com>
2025-04-05 11:57:07 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,10 +22,10 @@ const Index = () => {
       <VideoGallery />
 
       {/* Footer */}
-      <footer className="bg-gray-100 py-8">
+      <footer className="bg-gray-100 py-6 sm:py-8">
         <div className="container mx-auto px-4">
           <div className="text-center text-gray-600">
-            <p>&copy; {new Date().getFullYear()} Be Courageous. All rights reserved.</p>
+            <p className="text-sm sm:text-base">&copy; {new Date().getFullYear()} Be Courageous. All rights reserved.</p>
           </div>
         </div>
       </footer>
